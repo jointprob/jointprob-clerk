09adbd1c87106000a2109a6cfbab672044a21546
James Pratt <me@jamiep.org>
2022-08-12 09:00:03 +0200

    checking that my dbinomial is actually working

--- test/dbinomial_test.clj
+++ test/dbinomial_test.clj
@@ -5,7 +5,6 @@
 (deftest exp-test
   (is (= (exp 2.0 4.0) 16.0)))
 
-
 (deftest factorial-test
   (are [a fac] (= fac (factorial a))
                0 1
@@ -13,7 +12,10 @@
                2 2
                3 6))
 
-
-
 (deftest dbinom-test
-  (is (= (dbinom 6 9 0.5) 0.1640625)))
+  (are [x size prob result] (= (dbinom x size prob) result)
+                            6 9 0.5 0.1640625
+                            10 20 0.5 0.17619705200195312
+                            1  1  0 0
+                            1  1  1 1))
+
